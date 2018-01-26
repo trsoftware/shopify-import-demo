793537f78dd025fe814d40ce151e78d243d3fa1a
ost <ost@ost-desktop>
2018-01-26 09:48:59 +0200

    Moved Doctrine configuration to global

--- app/config/autoload/global.php
+++ app/config/autoload/global.php
@@ -1,4 +1,18 @@
 <?php
+use Doctrine\DBAL\Driver\PDOMySql\Driver as PDOMySqlDriver;
 
 return [
+     'doctrine' => [
+        'connection' => [
+            'orm_default' => [
+                'driverClass' => PDOMySqlDriver::class,
+                'params' => [
+                    'host'     => 'db',                    
+                    'user'     => 'root',
+                    'password' => 'root',
+                    'dbname'   => 'contest',
+                ]
+            ],            
+        ]    
+    ],  
 ];
